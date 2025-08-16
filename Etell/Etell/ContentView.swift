2fb55711fb9d019ca043afd849e1e5fa69cd7e3c
GeenethM <geenethmedagedara@gmail.com>
2025-08-16 10:55:37 +0530

    first commit

---
+++ Etell/Etell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Etell
+//
+//  Created by Geeneth 013 on 2025-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Etell/Etell/EtellApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtellApp.swift
+//  Etell
+//
+//  Created by Geeneth 013 on 2025-08-16.
+//
+
+import SwiftUI
+
+@main
+struct EtellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
